6b88b9dc47fa1225ce6aeed78434ac3a5ef3f225
Rustam-Ulaevich <praporglamur@gmail.com>
2021-09-26 11:51:25 +0500

    enit commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export function Header() {
+    return <header className='header'>
+        <img src='https://logos.textgiraffe.com/logos/logo-name/Rustam-designstyle-boots-m.png'/>
+    </header>
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export  function Profile() {
+    return(
+        <div className='content'>
+            <div>
+                <img src='https://срочный-выкуп-авто-в-волгограде.рф/wp-content/uploads/2019/12/photo-1457201099140-5a4cda31de82.jpg'/>
+            </div>
+            <div>ava+description</div>
+            <div>
+                My posts
+                <div> 
+                    New posts
+                </div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
